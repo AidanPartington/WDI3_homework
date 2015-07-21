ebada41e02e850d135ac9ef8df01394a20395463
AIDAN PARTINGTON <AidanPartington@gmail.com>
2015-07-22 09:56:51 +1000

    update

---
+++ Aidan_Partington/Warmups/robotName.rb
@@ -0,0 +1,36 @@
+# https://gist.github.com/mattswann/d816adfec6b56fb27d6c
+require 'pry'
+
+class Robot
+  
+  def initialize
+    name = []
+    alphabet = %w{A B C D E F G H I J K L M N O P Q R S T U V W X Y Z}
+    name[0] = alphabet[rand(26)]
+    name[1] = alphabet[rand(26)]
+    name[2] = rand(10)
+    name[3] = rand(10)
+    name[4] = rand(10)
+    name = name.join
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+end
+
+binding.pry
+
+# puts "Robot 1: "
+# robot1 = Robot.new
+# puts robot1.name
+# puts robot1.name
+# puts robot1.name
+
+# puts "Robot 2: "
+# robot2 = Robot.new
+# puts robot2.name
+# puts robot2.name
+# puts robot2.name

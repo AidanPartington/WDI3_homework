04894f13ed24905260ee26d6a46aa96971650fd2
AIDAN PARTINGTON <AidanPartington@gmail.com>
2015-07-22 10:06:44 +1000

    Robot warm up

--- Aidan_Partington/Warmups/robotName.rb
+++ Aidan_Partington/Warmups/robotName.rb
@@ -19,18 +19,39 @@ class Robot
     @name
   end
 
+  def reset
+    name = []
+    alphabet = %w{A B C D E F G H I J K L M N O P Q R S T U V W X Y Z}
+    name[0] = alphabet[rand(26)]
+    name[1] = alphabet[rand(26)]
+    name[2] = rand(10)
+    name[3] = rand(10)
+    name[4] = rand(10)
+    name = name.join
+    @name = name
+  end
+
 end
 
+puts "Robot 1: "
+robot1 = Robot.new
+puts robot1.name
+puts robot1.name
+puts robot1.name
+
+puts "Robot 2: "
+robot2 = Robot.new
+puts robot2.name
+puts robot2.name
+puts robot2.name
+
+puts "Robot 3: "
+robot3 = Robot.new
+puts robot3.name
+puts robot3.name
+puts "Resetting to factory settings."
+robot3.reset
+puts robot3.name
+puts robot3.name
+
 binding.pry
-
-# puts "Robot 1: "
-# robot1 = Robot.new
-# puts robot1.name
-# puts robot1.name
-# puts robot1.name
-
-# puts "Robot 2: "
-# robot2 = Robot.new
-# puts robot2.name
-# puts robot2.name
-# puts robot2.name
